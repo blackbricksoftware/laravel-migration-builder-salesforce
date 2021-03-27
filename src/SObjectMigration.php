fa3454508f554e552de40a40ed021839195c53ca
David Hayes <dhayes@lalgbtcenter.org>
2021-03-27 00:40:44 -0700

    Make all columns nullable

--- src/SObjectMigration.php
+++ src/SObjectMigration.php
@@ -25,6 +25,8 @@ class SObjectMigration extends Migration
       $translateMethod = $this->findSObjectFieldToColumnTranslation($field);
       $column = $this->$translateMethod($field);
 
+      $column->setNullable(true);
+
       if ($field->unique)
         $column->setUnique(true);
 
